09f837e9e972f3311296404836eb42e2e4163811
skdie <galabpokh@gmail.com>
2018-12-04 19:38:43 +0545

    fourth try

--- server.js
+++ server.js
@@ -22,7 +22,9 @@ io.on('connection', function(socket){
 });
 
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+// http.listen(3000, function(){
+//   console.log('listening on *:3000');
   
-});
+// });
+
+app.listen(3000);
