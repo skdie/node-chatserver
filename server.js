3f4cb80f69a1062ca4c0782ca0983c75ed9a219f
skdie <galabpokh@gmail.com>
2018-12-04 19:36:37 +0545

    third try

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.send("hello");
 });
 
 io.on('connection', function(socket){
